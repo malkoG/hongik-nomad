301af561d603d52e6cc6f9367a80e0396c3b9cea
KODING WARRIOR <rijgndqw012@gmail.com>
2018-12-27 06:30:08 +0900

    Implement Filling in form using chromium

--- lib/nomadic_crawler/crawler.rb
+++ lib/nomadic_crawler/crawler.rb
@@ -1,23 +1,51 @@
 require "selenium-webdriver"
+require "nokogiri"
+require "scanf"
 
 module NomadicCrawler
-    class Crawler
-        attr_reader :target_size
-        attr_accessor :driver
+  class Crawler
+    attr_reader :target_size
+    attr_accessor :driver
 
-        def initialize(target_site)
-            @target_site = target_site
+    def initialize(target_site)
+      @target_site = target_site
 
-            options = Selenium::WebDriver::Chrome::Options.new
-            options.add_argument('--headless')
-            @driver = Selenium::WebDriver.for :chrome, options: options
-            
-            @driver.navigate.to @target_site
-        end
-
-        def get_latest_semester
-            element = @driver.find_element tag_name: 'select'
-            return element
-        end
+      options = Selenium::WebDriver::Chrome::Options.new
+      options.add_argument('--headless')
+      @driver = Selenium::WebDriver.for :chrome, options: options
+      
+      @driver.navigate.to @target_site
     end
+
+    def fill_in_the_form
+      select_tag = Selenium::WebDriver::Support::Select.new(@driver.find_element tag_name: 'select')
+      select_tag.select_by(:index, 0)
+      year, semester = select_tag.selected_options[0].property(:value).scanf "%4d%1d"
+
+      checkbox    = @driver.find_element id: "input", name: "p_y2016"
+      checkbox.click
+      
+      radio_xpath  = '//*[@id="select_abeek"]/tbody/tr[2]/td/form/input[4]'
+      radio_button = @driver.find_element xpath: radio_xpath
+      radio_button.click
+
+      return [year, semester]
+    end
+
+    def request_latest_semester_curriculum
+      year, semester = fill_in_the_form
+      
+      submit_xpath = '//*[@id="select_abeek"]/tbody/tr[2]/td/form/input[7]'
+      submit_button = @driver.find_element xpath: submit_xpath
+      submit_button.click
+
+      sleep(3)
+
+      element = @driver.find_element id: 'table_seoul'
+
+
+
+      return element
+    end
+  end
 end
--- test/lib/nomadic_crawler/crawler_test.rb
+++ test/lib/nomadic_crawler/crawler_test.rb
@@ -1,4 +1,6 @@
 require 'test_helper'
+require 'selenium-webdriver'
+require 'nokogiri'
 
 module NomadicCrawler
     class CrawlerTest < ActiveSupport::TestCase
@@ -6,9 +8,17 @@ module NomadicCrawler
             @crawler = Crawler.new(ENV['TARGET_SITE'])
         end
 
+        test 'fill in the form' do
+            @crawler.fill_in_the_form
+
+            assert true # 최근 학기를 선택했는지
+            assert true # 언제 입학했는지 체크
+            assert true # 어떤 종류의 학생인지 체크
+        end
+
         test 'Get Latest Semester' do
-            element = @crawler.get_latest_semester
-            assert_equal element.tag_name, 'select'
+            @crawler.fill_in_the_form
+            @crawler.request_latest_semester_curriculum
         end
     end
 end
